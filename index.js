74cb72d62e07ecfae2de944934622373b6c9b6b5
Brittany Morris <brittmmendez@gmail.com>
2018-03-16 20:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function lowerCaseDrivers(drivers){
+  //returns all drivers lowercased
+  //does not modify the original array
   return drivers.map(function (driver) { return driver.toLowerCase();});
 }
 
 function nameToAttributes (drivers) {
+  //returns list of objects with appropriate first and last names
   return drivers.map(function (driver) {
     const splitName=driver.split(" ");
     return {firstName: splitName[0], lastName: splitName[1]};
